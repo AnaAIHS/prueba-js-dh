4c53f30bb918662dcd9051be06be050197e304b5
AnaAIHS <anahersor@gmail.com>
2021-05-18 11:24:54 -0500

    Cambios a app.js - una disculpita de antemano, andaba bien perdida con los JSON

--- app.js
+++ app.js
@@ -7,16 +7,33 @@
          )
 */
 
+let tareas = require('./tareas.json');
+const fs = require('fs');
+
 exports.leerTareas = function () {
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
-}
+    let lista = fs.readFileSync('./tareas.json', 'utf-8');
+    let nuevo = JSON.parse(lista);
+    console.log('leer');
+    console.log(nuevo);
+    return nuevo;
+    //return JSON.parse(lista);
+};
 
 exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
+   let lista = fs.readFileSync('./tareas.json', 'utf-8');
+   console.log(lista);
+    lista=JSON.parse(lista);
+    lista.push(tarea);
+    lista = JSON.stringify(lista);
+    console.log('agregar');
+    console.log(lista);
+    fs.writeFileSync('./tareas.json', lista, 'utf-8');
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -24,4 +41,10 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+   let lista = fs.readFileSync('./tareas.json', 'utf-8');
+   lista=JSON.parse(lista);
+    let filtrado = lista.filter(function(param){
+    return param.status == estado;
+    });
+    return filtrado;
 }
